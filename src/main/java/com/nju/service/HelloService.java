4d9f71a7e2cf6369d424a943e3eba87ca71b5bd4
maw-b <maw-b@glodon.com>
2021-08-24 17:44:34 +0800

    sonarqube 单元测试练习

--- src/main/java/com/nju/service/HelloService.java
+++ src/main/java/com/nju/service/HelloService.java
@@ -2,6 +2,7 @@ package com.nju.service;
 
 import org.springframework.stereotype.Service;
 
+
 /**
  * @author maw-b
  * @date 2021/8/16 14:03
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class HelloService {
 
-    public int add(int a,int b){
-        return a+b;
+    public int add(int a, int b) {
+        return a + b;
     }
 }
